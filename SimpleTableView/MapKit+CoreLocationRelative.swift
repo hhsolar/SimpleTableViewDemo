1767e8742d929f8dde7bf6be9dc766a414b3c999
HerbertHong <hhsolar@gmail.com>
2017-07-18 22:05:40 -0700

    Finish the StartViewController

---
+++ SimpleTableView/MapKit+CoreLocationRelative.swift
@@ -0,0 +1,69 @@
+//
+//  MapKit+CoreLocationRelative.swift
+//  DoorDashThird
+//
+//  Created by apple on 12/7/2017.
+//  Copyright © 2017 greatwall. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+extension StartViewController {
+    
+    // MARK: Annotation
+    internal func addAnnotation(with coordinate: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        //        annotation.title =
+        //        annotation.subtitle =
+        if mapView.annotations.count > 0 {
+            mapView.removeAnnotations(mapView.annotations)
+        }
+        mapView.addAnnotation(annotation)
+    }
+    
+    // MARK: Convert coordinate to address
+    internal func covertToAddress(from coordinate: CLLocationCoordinate2D) {
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        geocode.reverseGeocodeLocation(location, completionHandler: { [weak weakself = self]
+            (placemarks: [CLPlacemark]?, error: Error?) in
+            if error != nil {
+                print("Error happened when convert to address: \(String(describing: error))")
+                return
+            }
+            
+            let placemark = placemarks?.first
+            weakself?.addressLabel.text = self.stringFromPlacemark(placemark!)
+            weakself?.confirmButton.isEnabled = true
+            weakself?.confirmButton.setTitleColor(UIColor.white, for: .normal)
+        })
+    }
+    
+    
+    // MARK: MKMapViewDelegate
+    
+    
+    
+    private func stringFromPlacemark(_ placemark: CLPlacemark) -> String {
+        var text = ""
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        if let s = placemark.locality {
+            text += s + ", "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s
+        }
+        return text
+    }
+    
+}
--- SimpleTableView/StartViewController.swift
+++ SimpleTableView/StartViewController.swift
@@ -8,15 +8,99 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class StartViewController: UIViewController, MKMapViewDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.mapType = .standard
+            mapView.showsUserLocation = true
+        }
+    }
 
     @IBOutlet weak var addressLabel: UILabel!
 
-    @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var confirmButton: UIButton! {
+        didSet {
+            confirmButton.isEnabled = false
+        }
+    }
     
+    lazy var geocode: CLGeocoder = {
+        return CLGeocoder()
+    }()
     
+    private var forPassCoordinate = CLLocationCoordinate2D()
+
+    private let locationManager = CLLocationManager()
+
+    @IBAction func chooseALocation(_ sender: UILongPressGestureRecognizer) {
+        sender.minimumPressDuration = Constants.minPressDuration
+        sender.allowableMovement = Constants.longestMovement
+        
+        if sender.state == .ended {
+            return
+        }
+        
+        let touchPoint = sender.location(in: mapView)
+        let touchMapCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        forPassCoordinate = touchMapCoordinate
+        
+        addAnnotation(with: touchMapCoordinate)
+        covertToAddress(from: touchMapCoordinate)
+    }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Choose an Address"
+        confirmButton.setTitleColor(UIColor.lightText, for: .normal)
+        
+        let authStatus = CLLocationManager.authorizationStatus()
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        let userLocationCoordinate = getUserLocation(check: authStatus)
+        
+        if userLocationCoordinate != nil {
+            covertToAddress(from: userLocationCoordinate!)
+            let region = MKCoordinateRegionMakeWithDistance(userLocationCoordinate!, Constants.latitudinalMeterScale, Constants.longitudinalMeterScale)
+            mapView.setRegion(region, animated: true)
+            forPassCoordinate = userLocationCoordinate!
+        }
+    }
+    
+    private func getUserLocation(check authStatus: CLAuthorizationStatus) -> CLLocationCoordinate2D? {
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            let latitude = (locationManager.location?.coordinate.latitude)!
+            let longitude = (locationManager.location?.coordinate.longitude)!
+            
+            return CLLocationCoordinate2DMake(latitude, longitude)
+        }
+        return nil
+    }
+
+    private struct Constants {
+        static let minPressDuration = 0.5
+        static let longestMovement = CGFloat(10.0)
+        static let latitudinalMeterScale: CLLocationDegrees = 1000
+        static let longitudinalMeterScale: CLLocationDegrees = 1000
+    }
 }
+
+extension UIViewController {
+    var content: UIViewController {
+        if let navigationController = self as? UINavigationController {
+            return navigationController.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+}
+
+
+
+
+
