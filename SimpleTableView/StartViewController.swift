40a26a29d69fb0a61aeb01a79deafaf9a01cf146
HerbertHong <hhsolar@gmail.com>
2017-07-18 21:51:37 -0700

    Init StartViewController

---
+++ SimpleTableView/StartViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StartViewController.swift
+//  SimpleTableView
+//
+//  Created by apple on 18/7/2017.
+//  Copyright © 2017 greatwall. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class StartViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+    @IBOutlet weak var addressLabel: UILabel!
+
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    
+    
+}
