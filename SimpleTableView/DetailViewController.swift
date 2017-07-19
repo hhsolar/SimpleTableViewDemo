efce2ebfe2db6ef85dda580421389d2155b0fb17
HerbertHong <hhsolar@gmail.com>
2017-07-18 23:50:06 -0700

    additional commit

---
+++ SimpleTableView/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  SimpleTableView
+//
+//  Created by apple on 18/7/2017.
+//  Copyright © 2017 greatwall. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var passShop: Shop?
+
+
+}
